77d754a9406201526dca5e63c8ab583e950888ef
sandmark <sandmark.m@gmail.com>
2016-01-30 20:49:41 +0900

    Fix FB condition checking.
    Solved.

--- src/01-fizzbuzz.hs
+++ src/01-fizzbuzz.hs
@@ -25,7 +25,7 @@ string2int s = read s :: Int
 
 fizzbuzz :: Int -> Int -> Int -> String
 fizzbuzz f b n = unwords $ map fb [1..n]
-  where fb m | m `mod` (f*b) == 0 = "FB"
-             | m `mod` f     == 0 = "F"
-             | m `mod` b     == 0 = "B"
-             | otherwise          = show m
+  where fb m | m `mod` f == 0 && m `mod` b == 0 = "FB"
+             | m `mod` f                   == 0 = "F"
+             | m `mod` b                   == 0 = "B"
+             | otherwise                        = show m
